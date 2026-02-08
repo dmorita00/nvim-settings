05d6084a120058f210e29c6710af075b85b67356
dmorita00 <ingressmrt@gmail.com>
2026-02-09 00:45:28 +0900

    add git commands

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -28,3 +28,21 @@ map('n', '<Leader>d', ':lua vim.diagnostic.open_float()<CR>', { noremap = true }
 -- rebind embeded command
 map('n', '<C-_>', 'gcc', { noremap = false })
 map('v', '<C-_>', 'gc', { noremap = false })
+map('i', 'jj', '<Esc>', { noremap = true, silent = true })
+
+-- Git (fugitive)
+map('n', '<Leader>gg', ':Git<CR>', { noremap = true, silent = true })
+map('n', '<Leader>gc', ':Git commit<CR>', { noremap = true, silent = true })
+map('n', '<Leader>gp', ':Git push<CR>', { noremap = true, silent = true })
+map('n', '<Leader>gl', ':Git pull<CR>', { noremap = true, silent = true })
+map('n', '<Leader>gb', ':Git blame<CR>', { noremap = true, silent = true })
+-- map('n', '<Leader>gd', ':Git diff<CR>', { noremap = true, silent = true })
+map('n', '<Leader>gd', ':Gvdiffsplit<CR>', { noremap = true, silent = true })
+map('n', '<Leader>ga', ':Gwrite<CR>', { noremap = true, silent = true })
+map('n', '<Leader>gss', ':Git stash push -m ""<Left>', { noremap = true })
+map('n', '<Leader>gsa', ':Git stash apply<CR>', { noremap = true, silent = true })
+map('n', '<Leader>gP', ':Git commit<CR>:Git push<CR>', { noremap = true, silent = true })
+
+-- GitHub (rhubarb)
+map('n', '<Leader>go', ':GBrowse<CR>', { noremap = true, silent = true })
+map('v', '<Leader>go', ':GBrowse<CR>', { noremap = true, silent = true })
