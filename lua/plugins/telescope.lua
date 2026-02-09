f8f9db1769b95459f00635b0dd6f850e52be4c9e
dmorita00 <ingressmrt@gmail.com>
2026-02-10 01:26:49 +0900

    Change to find command

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,7 +4,7 @@ return {
   priority = 10000,
   keys = {
     { "<leader>p", "<cmd>Telescope find_files<cr>", desc = "Telescope find_files" },
-    { "<leader>g", "<cmd>Telescope live_grep<cr>", desc = "Telescope live_greps" },
+    { "<leader>ff", "<cmd>Telescope live_grep<cr>", desc = "Telescope live_greps" },
     { "<leader>r", "<cmd>Telescope resume<cr>", desc = "Telescope Resume" },
     { "<leader>b", "<cmd>Telescope pickers<cr>", desc = "Telescope Previous Picker" },
     { "<leader>f", "<cmd>Telescope quickfix<cr>", desc = "Telescope Quixfix" },
